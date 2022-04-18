55bfa55a4ff9a136473b102f9b2a920ff873861f
Nifraz Navahz <nifraz@live.com>
2022-04-18 13:15:50 +0530

    Add initial migration

---
+++ Cot.Web/Migrations/20220418073017_AddCourseTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cot.Web.Migrations
+{
+    public partial class AddCourseTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Code = table.Column<string>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Code);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Courses");
+        }
+    }
+}

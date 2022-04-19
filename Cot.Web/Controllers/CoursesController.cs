1c29f24ff628e92c191e72508272069c7593dfd8
Nifraz Navahz <nifraz@live.com>
2022-04-19 16:55:43 +0530

    Add course controller with views

---
+++ Cot.Web/Controllers/CoursesController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Cot.Web.Core.Domain;
+using Cot.Web.Persistence;
+using Cot.Web.Core;
+
+namespace Cot.Web.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly CotDbContext context;
+        private readonly IUnitOfWork unitOfWork;
+
+        public CoursesController(CotDbContext context, IUnitOfWork unitOfWork)
+        {
+            this.context = context;
+            this.unitOfWork = unitOfWork;
+        }
+
+        // GET: Courses
+        public async Task<IActionResult> Index()
+        {
+            return View(await context.Courses.ToListAsync());
+        }
+
+        // GET: Courses/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await context.Courses
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Courses/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Courses/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Code,Title")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Add(course);
+                await context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await context.Courses.FindAsync(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            return View(course);
+        }
+
+        // POST: Courses/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Code,Title")] Course course)
+        {
+            if (id != course.Code)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    context.Update(course);
+                    await context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.Code))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Courses/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await context.Courses
+                .FirstOrDefaultAsync(m => m.Code == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Courses/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var course = await context.Courses.FindAsync(id);
+            context.Courses.Remove(course);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(string id)
+        {
+            return context.Courses.Any(e => e.Code == id);
+        }
+    }
+}

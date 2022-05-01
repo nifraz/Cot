7e7af299b8bd25aecccdc3ad31c5dd8d0c27998c
Nifraz Navahz <nifraz@live.com>
2022-05-01 11:20:56 +0530

    Add and use StringFormat.cs

---
+++ Cot.Web/Utilities/StringFormat.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cot.Web.Utilities
+{
+    public static class StringFormat
+    {
+        public const string DateTime = "yyyy/MM/dd | hh:mm:ss tt";
+    }
+}

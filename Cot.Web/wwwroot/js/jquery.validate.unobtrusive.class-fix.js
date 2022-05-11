920e0075f4d01d66f3ef3010ad661cba51ca58f4
Nifraz Navahz <nifraz@live.com>
2022-05-11 11:56:08 +0530

    Fix input validation class issue

---
+++ Cot.Web/wwwroot/js/jquery.validate.unobtrusive.class-fix.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('.input-validation-error').addClass('is-invalid').removeClass('input-validation-error');
+});
+var settings = {
+    validClass: "is-valid",
+    errorClass: "is-invalid"
+};
+$.validator.setDefaults(settings);
+$.validator.unobtrusive.options = settings;
